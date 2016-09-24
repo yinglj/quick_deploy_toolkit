de523c32278886dad2bb3781d6e4d6c85687f7cd
yinglj <yinglj@gmail.com>
2016-09-24 18:14:48 +0800

    format remote_shell.py
    format remote_shell.py

--- remote_shell.py
+++ remote_shell.py
@@ -21,97 +21,103 @@ import pexpect
 
 class remote_shell(cmd.Cmd):
 
-	def __init__(self):
-		cmd.Cmd.__init__(self)
-		#self.intro = '''Enter \"help\" for instructions'''
-		self.prompt = 'remote shell>'
-		self.secs = 1.0
-		self.count = 3
-		self.his = []
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        #self.intro = '''Enter \"help\" for instructions'''
+        self.prompt = 'remote shell>'
+        self.secs = 1.0
+        self.count = 3
+        self.his = []
 
-	def quit(self):
-		try:
-			self.child.communicate('exit')
-		except:
-			pass
+    def quit(self):
+        try:
+            self.child.communicate('exit')
+        except:
+            pass
 
-	def do_prompt(self, line):
-		'''Set command prompt, eg. \"prompt remote shell\"'''
-		self.prompt = 'remote shell>'
+    def do_prompt(self, line):
+        '''Set command prompt, eg. \"prompt remote shell\"'''
+        self.prompt = 'remote shell>'
 
-	def do_EOF(self, line):
-		'''Exit remote_shell.py with EOF.'''
-		print
-		return 1
+    def do_EOF(self, line):
+        '''Exit remote_shell.py with EOF.'''
+        print
+        return 1
 
-	def emptyline(self):
-		pass
+    def emptyline(self):
+        pass
 
-	def do_exit(self, line):
-		'''Exit remote_shell.py.'''
-		self.quit()
-		return True
+    def do_exit(self, line):
+        '''Exit remote_shell.py.'''
+        self.quit()
+        return True
 
-	def do_bye(self, line):
-		'''Exit remote_shell.py.'''
-		self.quit()
-		return True
+    def do_bye(self, line):
+        '''Exit remote_shell.py.'''
+        self.quit()
+        return True
 
-	def do_quit(self, line):
-		'''Exit remote_shell.py.'''
-		self.quit()
-		return True
+    def do_quit(self, line):
+        '''Exit remote_shell.py.'''
+        self.quit()
+        return True
 
-	def do_by(self, line):
-		'''Exit remote_shell.py.'''
-		self.quit()
-		return True
+    def do_by(self, line):
+        '''Exit remote_shell.py.'''
+        self.quit()
+        return True
 
-	def onecmd(self, line):
-		'''Execute the rest of the line as a shell command, eg. \'!ls\', \'shell pwd\'.'''
-		if line == "" or line == "bye" or line == "exit" or line == "by" or line == "quit" \
-			or "help" in line or line == "EOF" or "shell" in line or "run" in line:
-			return cmd.Cmd.onecmd(self, line)	
-		if line != "" and line[0] == '!':
-			if len(line)>2 and line[1:3] == "vi":
-				print "can't support !vi or !vim"
-				return False
-			command = subprocess.Popen(line[1:], shell=True, stdout=subprocess.PIPE)
-			print command.communicate()[0],
-			return False
-		szCmd = os.path.dirname(os.path.realpath(__file__))+"/remote_cmd.py "+line
-		print szCmd;
-		command = subprocess.Popen(szCmd, shell=True, stdout=subprocess.PIPE)
-		print command.communicate()[0],
-		pass
-		
-	def do_shell(self, line):
-		'''Execute the rest of the line as a shell command, eg. \'!ls\', \'shell pwd\'.
-	! for localhost, shell or none for remote host.'''
-		szCmd = os.path.dirname(os.path.realpath(__file__))+"/remote_cmd.py "+line
-		print szCmd;
-		command = subprocess.Popen(szCmd, shell=True, stdout=subprocess.PIPE)
-		print command.communicate()[0],
+    def onecmd(self, line):
+        '''Execute the rest of the line as a shell command, eg. \'!ls\', \'shell pwd\'.'''
+        if line == "" or line == "bye" or line == "exit" or line == "by" or line == "quit" \
+            or "help" in line or line == "EOF" or "shell" in line or "run" in line:
+            return cmd.Cmd.onecmd(self, line)
+        if line != "" and line[0] == '!':
+            if len(line)>2 and line[1:3] == "vi":
+                print "can't support !vi or !vim"
+                return False
+            command = subprocess.Popen(line[1:], shell=True, stdout=subprocess.PIPE)
+            print command.communicate()[0],
+            return False
 
-	def do_run(self, line):
-		'''Execute the rest of the line as a shell command, eg. \'run ls\', \'run pwd\'.'''
-		szCmd = os.path.dirname(os.path.realpath(__file__))+"/remote_cmd.py "+line
-		print szCmd;
-		command = subprocess.Popen(szCmd, shell=True, stdout=subprocess.PIPE)
-		print command.communicate()[0],
+        if len(line)>1 and line[0:2] == "vi":
+            print "can't support vi or vim. Interactive command is so on."
+            return False
+        remote_cmd(line)
+        pass
+
+    def do_shell(self, line):
+        '''Execute the rest of the line as a shell command, eg. \'!ls\', \'shell pwd\'.
+    ! for localhost, shell or none for remote host.'''
+        remote_cmd(line)
+
+    def do_run(self, line):
+        '''Execute the rest of the line as a shell command, eg. \'run ls\', \'run pwd\'.'''
+        remote_cmd(line)
+
+def remote_cmd(line):
+        szCmd = os.path.dirname(os.path.realpath(__file__))+"/remote_cmd.py "+line
+        print szCmd;
+        command = subprocess.Popen(szCmd, shell=True, stdout=subprocess.PIPE)
+        while 1:
+            out = command.stdout.readline()
+            if out == '':
+                break
+            print out,
+        #print command.communicate()[0],
 
 if __name__ == '__main__':
-		if len(sys.argv) > 1:
-			print "usage:",sys.argv[0]
-		else:
-			client = remote_shell()
-			try:
-				client.cmdloop()
-			except KeyboardInterrupt as e:
-				print e
-			except IOError as e:
-				print e
-			except ValueError as e:
-				print e
-			finally:
-				client.quit()
+        if len(sys.argv) > 1:
+            print "usage:",sys.argv[0]
+        else:
+            client = remote_shell()
+            try:
+                client.cmdloop()
+            except KeyboardInterrupt as e:
+                print e
+            except IOError as e:
+                print e
+            except ValueError as e:
+                print e
+            finally:
+                client.quit()
